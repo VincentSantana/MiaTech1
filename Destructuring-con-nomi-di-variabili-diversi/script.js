13a1cb3824ba3ad2acc64865bd35c9474797a076
VincentSantana <v.santanab13@gmail.com>
2024-11-24 21:50:08 +0100

    aggiunti esercizi

---
+++ Destructuring-con-nomi-di-variabili-diversi/script.js
@@ -0,0 +1,11 @@
+const persona = {
+    nome: "Vincent",
+    età: 27,
+    città: "Barahona"
+};
+
+const { nome: nombre, età: edad, città: ciudad } = persona;
+
+console.log(nombre); 
+console.log(edad);  
+console.log(ciudad);
---
+++ Estrazione-di-proprieta-base/script.js
@@ -0,0 +1,11 @@
+const persona = {
+    nome: "Vincent",
+    età: 27,
+    città: "Barahona"
+};
+
+const { nome, età, città } = persona;
+
+console.log(nome); 
+console.log(età);  
+console.log(città);
