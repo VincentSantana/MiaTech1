a8cb5b9e9d66ec489275aafe81e23c5e78130f82
VincentSantana <v.santanab13@gmail.com>
2024-11-28 19:30:56 +0100

    esercizi modificati

--- Funzione-di-base-callback/script.js
+++ Funzione-di-base-callback/script.js
@@ -1,10 +1,11 @@
 // creo una funzione in cui callback fa il lavoro di un return
 function somman(valore1, valore2, callback){
-    callback(valore1 + valore2)
+   let risultato = valore1 + valore2;
+   console.log("risultato somma: " + risultato);
+//callback eseguito
+   callback(risultato);
 }
 
-// asegnando i valori, metto poi la funzione dichiarando all'interno una variabile chiamata risultato che contiene il risultato dei valori
-somman(7, 6, function(risultato) {
-    //facendo il console su risultato si vede poi la somma dei valori
-    console.log("risultato:" + risultato)
-})
+somman(7, 6, function() {
+    console.log("operazione eseguita")
+});
--- creare-una-promessa-semplice/script.js
+++ creare-una-promessa-semplice/script.js
@@ -1,13 +1,14 @@
 //creazione della funzione che restituisce una promessa
-let promessaConMessaggio = new Promise(function(resolve, reject) {
-      setTimeout(() => {
-        resolve("La promessa è stata risolta dopo 2 secondi!");
-      }, 2000);
-    });
-  
+function promessaConMessaggio() {
+  return new Promise(function(resolve, reject) {
+    setTimeout(() => {
+      resolve("La promessa è stata risolta dopo 2 secondi!");
+    }, 2000);
+  });
+}
+
   //lancio il risultato della promessa
-  promessaConMessaggio.then(
+  promessaConMessaggio().then(
     function(messaggio) {
       console.log(messaggio);
     });
-  
