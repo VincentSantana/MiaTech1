2d542751cd1d7df42e9da9d9f909810268ae3f38
VincentSantana <v.santanab13@gmail.com>
2024-12-16 19:56:33 +0100

    inizio esercizi react

---
+++ test-project/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  const title = "Hello World";
+  const text = "where are you from?";
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{text}</p>
+    </>
+  )
+}
+
+export default App
