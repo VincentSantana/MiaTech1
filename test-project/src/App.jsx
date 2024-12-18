b6c2eafe803cf2134c2a417e916ac6ddef23b0c6
VincentSantana <v.santanab13@gmail.com>
2024-12-18 18:45:18 +0100

    aggiornamento

--- test-project/src/App.jsx
+++ test-project/src/App.jsx
@@ -1,13 +1,33 @@
-function App() {
-  const title = "Hello World";
-  const text = "where are you from?";
+import Card from "./components/Card";
 
-  return (
-    <>
-      <h1>{title}</h1>
-      <p>{text}</p>
-    </>
-  )
+const cards = [
+    {
+        title: "My Title 1",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis excepturi repudiandae impedit? At dolor, fugiat, asperiores eius repellendus velit recusandae ipsam quia, commodi doloribus optio pariatur possimus facere praesentium nesciunt?"
+    },
+    {
+        title: "My Title 2",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis excepturi repudiandae impedit? At dolor, fugiat, asperiores eius repellendus velit recusandae ipsam quia, commodi doloribus optio pariatur possimus facere praesentium nesciunt?"
+    },
+    {
+        title: "My Title 3",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis excepturi repudiandae impedit? At dolor, fugiat, asperiores eius repellendus velit recusandae ipsam quia, commodi doloribus optio pariatur possimus facere praesentium nesciunt?"
+    },
+    {
+        title: "My Title 4",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis excepturi repudiandae impedit? At dolor, fugiat, asperiores eius repellendus velit recusandae ipsam quia, commodi doloribus optio pariatur possimus facere praesentium nesciunt?"
+    },
+];
+
+const App = () => {
+    return (
+        <>
+            <Card content={cards[0]} />
+            <Card content={cards[1]} />
+            <Card content={cards[2]} />
+            <Card content={cards[3]} />
+        </>
+    )
 }
 
-export default App
+export default App;
