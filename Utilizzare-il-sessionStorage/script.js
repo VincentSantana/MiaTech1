3ba9c2f0850347446f814a5509090d4b0532f3e8
VincentSantana <v.santanab13@gmail.com>
2024-12-19 09:56:39 +0100

    esercizio js fatto

--- Utilizzare-il-sessionStorage/script.js
+++ Utilizzare-il-sessionStorage/script.js
@@ -4,14 +4,14 @@ function salvaValore(chiave, valore) {
     console.log(`Valore salvato nel localStorage: ${chiave} = ${valore}`);
   }
   
-  // Funzione per recuperare un valore dal localStorage
+  // Funzione per recuperare un valore dal sessionStorage
   function recuperaValore(chiave) {
     const valore = sessionStorage.getItem(chiave);
     console.log(`Valore recuperato dal localStorage: ${chiave} = ${valore}`);
     return valore;
   }
   
-  // Funzione per rimuovere un valore dal localStorage
+  // Funzione per rimuovere un valore dal sessionStorage
   function rimuoviValore(chiave) {
     sessionStorage.removeItem(chiave);
     console.log(`Valore rimosso dal localStorage: ${chiave}`);
