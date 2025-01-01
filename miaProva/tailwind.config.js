1e7386fbeda5194a626b7349e2a68b21ded47d60
VincentSantana <v.santanab13@gmail.com>
2025-01-01 21:10:39 +0100

    alcuni esercizi

---
+++ miaProva/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3498db',
+        secondary: '#f1c40f',
+        success: '#2ecc71',
+        danger: '#e74c3c',
+        warning: '#f7dc6f',
+      },
+    },
+  },
+};
