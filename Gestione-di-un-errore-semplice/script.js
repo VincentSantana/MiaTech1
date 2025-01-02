6d9a4928cbe26d1b4d21d1f58bcf5456ee9254ec
VincentSantana <v.santanab13@gmail.com>
2025-01-02 20:23:26 +0100

    esercizio risolto

--- Gestione-di-un-errore-semplice/script.js
+++ Gestione-di-un-errore-semplice/script.js
@@ -1,8 +1,15 @@
 //non rompe il flusso del programma
 try {
   const divisore = 0;
-  const risultato = 10 / divisore;
-  console.log(risultato);
+  const numb = 10;
+
+  //controllo che il divisore non sia zero
+  if (divisore === 0) {
+    throw new Error('Divisore non valido');// throw serve per lanciare un'eccezione, se la condizione è verificata, l'eccezione viene gestita dal blocco catch
+  }
+  //operazione con il divisore
+  console.log(numb / divisore);
+  
 } catch (err) {
   console.error('Errore: il divisore non può essere zero');
 }
