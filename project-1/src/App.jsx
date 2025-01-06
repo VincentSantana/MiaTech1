a72c4dbcc3e95a0dc27aadb97abb4b0e385a65d1
VincentSantana <v.santanab13@gmail.com>
2025-01-06 18:38:34 +0100

    esercizi react fatti

---
+++ project-1/src/App.jsx
@@ -0,0 +1,15 @@
+const Title = () => {
+  return (
+      <h1>Hello World!!</h1>
+  
+  );
+}
+const App = () => {
+  return (
+    <div>
+      <Title />
+    </div>
+  );
+};
+
+export default App; 
