22387e09e08df712473f7af8288685c3d0a52808
VincentSantana <v.santanab13@gmail.com>
2025-01-08 00:01:48 +0100

    modifica fatta

--- Submit/script.js
+++ Submit/script.js
@@ -1,13 +1,13 @@
 const form = document.getElementById("form");
 
 form.addEventListener("submit", function(event) {
+    event.preventDefault();
     const name = document.getElementById("fname").value;
     const surname = document.getElementById("lname").value;
-    if (name === "" , surname === "") {
+    if (name === "" && surname === "") {
         alert("Compila tutti i campi");
     } else {
         form.submit();
     }
     
 });
-
