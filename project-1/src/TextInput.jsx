4ea90d9693af98881cb0a324b82bd3f7b4f9d20a
VincentSantana <v.santanab13@gmail.com>
2025-01-14 17:39:45 +0100

    modifiche

--- project-1/src/TextInput.jsx
+++ project-1/src/TextInput.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 
 const TextInput = () => {
-  const [value, setValue] = useState('');  // Stato per mantenere il valore dell'input
+  const [value, setValue] = useState('');  // Stato per mantenere il valore dell'input(mantiene i dati)
 
-
+                      //event contiene il target, elemento che subisce l'evento click
   const handleChange = (event) => {  // Funzione per aggiornare lo stato quando cambia l'input
     setValue(event.target.value);
-    console.log(event.target.value)
+    console.log(event.target.value) //si vedono i valori su console log del testo che viene scritto in input
   };
 
   return (
