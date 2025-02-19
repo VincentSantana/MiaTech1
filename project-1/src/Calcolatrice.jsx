672150d683c506b6a6b02b4ec365af5c67cbfea6
VincentSantana <v.santanab13@gmail.com>
2025-02-19 22:08:53 +0100

    esercizio fatto

--- project-1/src/Calcolatrice.jsx
+++ project-1/src/Calcolatrice.jsx
@@ -3,17 +3,22 @@ import { useState } from "react"
 
 //fare 2 input che quando inserisco dei numeri con bottone si sommano i 2
 export default function Calcolatrice() {
-   const [somma, setSommaA] = useState(0);
+   const [sommaA, setSommaA] = useState(0); //si devono fare 3 stati perche il useState restituisce solo 2 valori
+   const [sommaB, setSommaB] = useState(0);
+   const [somma, setSomma] = useState(0);
 
+function handleSomma() {
+    setSomma(Number(sommaA) + Number(sommaB)); //Number() converte gli input in numeri
+};
 
-
+    //onChange aggiorna lo stato
     return (
-        <>
+        <>   
         <label htmlFor="somma_a">somma A:</label>
-        <input type="text" />
+        <input type="number" value={sommaA} onChange={(e) => setSommaA(e.target.value)} /> 
         <label htmlFor="somma_b">somma B:</label>
-        <input type="text" />
-        <button>somma</button>
+        <input type="text" value={sommaB} onChange={(e) => setSommaB(e.target.value)}/>
+        <button onClick={handleSomma}>somma</button>
         <div>{somma}</div>
         </>
 
