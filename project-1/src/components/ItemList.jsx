c69ecba9505ccc9b8a7a034c6bbd21297fb9c1b9
VincentSantana <v.santanab13@gmail.com>
2025-02-28 08:33:26 +0100

    fatto

--- project-1/src/components/ItemList.jsx
+++ project-1/src/components/ItemList.jsx
@@ -1,11 +1,32 @@
-
+import { useEffect, useRef, useState } from "react"
 
 export default function ItemList({item}) {
+    const [counter, setCounter] = useState(0)
+    const counterRef = useRef(0)
 
+    function List(){
+        console.log("Ciao")
+    }
+
+    function increment() {
+        setCounter((_counter) => {
+            return _counter + 1;
+        })
+    }
+
+    function incrementR() {
+       counterRef.current += 1
+       alert('You clicked ' + counterRef.current + ' times!');
+    }
+
+
+    useEffect(() => List(),
+[])// ogni volta che viene cliccato lui si aggiorna, solo quando viene cliccato, se le quadre sono vuote si carica solo 1 volta
 
     return (
         <>
-        <li>{item}</li>
-        
+        <li>{item} <button onClick={incrementR}>x</button></li>
+        <div>{counterRef.current}</div>
+
         </>)
 }
